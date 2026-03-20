585859d6371450e6d8ef6169551b60c1c610ebf1
Tran Hong Gam <honggamdx2@gmail.com>
2026-03-20 22:45:00 +0700

    feat: comment deep learn

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,3 +8,37 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
+/** Decorator chạy 1 app module,  trong app module có 1 Decorator
+ *  nhậm vào 1 param, param là 1 object gồm 3 thuộc tính
+ *  import | controller(App controller) | provider(App service)
+ *  Controller: Liên quan đến việc Routing, ví dụ có method get
+ * @Controller()
+    export class AppController {
+      constructor(private readonly appService: AppService) {}
+    
+      @Get()
+      getHello(): string {
+        return this.appService.getHello();
+      }
+    }
+ * trong controller có 1 Decorator để quuy định method getHello
+ * là method Get
+ * 
+ * trong method Get có this.appService.getHello(), đây là app service
+ * hay còn gọi là  nó inject vào
+ * 
+ * 
+ * @Injectable()
+    export class AppService {
+      getHello(): string {
+        return 'Hello World!';
+      }
+    }
+  Dùng decorator Injectable : có tác dụng inject vào những cái class khác
+
+  file app.controller.spec.ts => Testing thôi
+
+  Muốn chạy ở dev có dev mode thì chạy bằng npm run start dev
+ * 
+ * **/
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule); //Dòng code này trả về một đối tượng (instance) thực thi giao diện INestApplication.
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+// NestFactory:	Một lớp (class) core của NestJS cung cấp //
+//              các phương thức tĩnh để tạo một instance của ứng dụng.
+/**
+ * AppModule	Module gốc (root module) của ứng dụng.
+ *             NestJS sẽ dựa vào đây để quét và //
+ *              khởi tạo toàn bộ các module, controller, và provider con.
+ * /
+ * NestFactory.create() :      Hàm khởi tạo. Nó đọc AppModule, /
+ *        giải quyết các phụ thuộc (Dependency Injection) và trả về đối tượng app.
+ * /
+ * INestApplication	: * Một interface định nghĩa các phương thức để quản lý ứng dụng /
+ *                  (như listen, use, connectMicroservice).
+ * app.listen(): 	Kích hoạt HTTP server để bắt đầu lắng nghe các yêu cầu từ client.
+ * /
+ */
