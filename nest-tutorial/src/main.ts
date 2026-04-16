05830fdf05172f89b9bbf7807ff3b66cf11108cc
Tran Hong Gam <honggamdx2@gmail.com>
2026-04-16 11:03:22 +0700

    feat: setup port 3001 and 0.0.0.0 for deployment

--- nest-tutorial/src/main.ts
+++ nest-tutorial/src/main.ts
@@ -5,9 +5,10 @@ import { ValidationPipe } from './validation.pipe';
 // import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3001;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
